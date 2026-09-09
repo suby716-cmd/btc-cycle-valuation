55d1c647ba6951374fb2c16b88504597c3e2a208
suby716-cmd <suby716@gmail.com>
2026-09-09 15:36:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "btc-radar-v11";
+const CACHE = "btc-radar-v12";
 const ASSETS = ["./", "./index.html", "./cycle.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
